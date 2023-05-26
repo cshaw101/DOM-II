62bd679e391a696eff4dcad49eb4188f062cad52
caleb <shawcaleb1688@gmail.com>
2023-05-26 17:23:56 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//1-load
+window.onload = function(evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO!!'
+
+//2-copy
+    window.addEventListener('copy', () =>{
+        navigator.clipboard.readText()
+        .then(text => {
+            heading.textContent += text;
+        })
+    })
+
+//3-click - this can be used to spply a style when clicked and then disapear when clicked again
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+
+//dblClick
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = ''
+})
+
+// 5-keydown
+window.addEventListener('keydown', evt => {
+    if (evt.key == 6) {
+        document.body.innerHTML = '<h1>YOU RAN ORDER 66</h1>'
+    }
+})
+
+//6 - mousemove
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY} = evt
+    //console.log(`mouse is at ${clientX}, ${clientY}`)
+})
+
+//7-mouseenter
+//8-mouseleave
+
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+       destination.getElementsByClassName.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () =>{
+        destination.getElementsByClassName.fontWeight = 'initial'
+    })
+}
+}
+
